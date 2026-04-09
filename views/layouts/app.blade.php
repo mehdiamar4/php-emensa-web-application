9d7ca0f7bb645c130da127b9b6bd30a3cb3605d3
Malek Bouaziz <malek.bouaziz@alumni.fh-aachen.de>
2026-04-09 17:47:37 +0200

    Add files via upload

--- views/layouts/app.blade.php
+++ views/layouts/app.blade.php
@@ -1,1 +1,25 @@
+<!doctype html>
+<html lang="DE">
+    <head>
+        <meta charset="utf-8">
+        <title>E-Mensa Routing Script</title>
+        <meta name="description" content="yet another small MVC script">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/default.min.css">
 
+        @yield("cssextra")
+
+        <meta name="theme-color" content="#dadada">
+        <!-- good developers check the markup ;) -->
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                @yield("content")
+            </div>
+        </div>
+
+    @yield("jsextra")
+
+    </body>
+</html>
