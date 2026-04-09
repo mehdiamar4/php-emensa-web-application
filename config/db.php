3abda193e2af0f077359bc903cc6f8c07945c18a
Malek Bouaziz <malek.bouaziz@alumni.fh-aachen.de>
2026-04-09 17:43:07 +0200

    Add files via upload

--- config/db.php
+++ config/db.php
@@ -1,1 +1,15 @@
+<?php
 
+$db_host = "localhost"; // or '127.0.0.1'
+$db_user = 'root';
+$db_pass = 'azerty'; // put your password here
+$db_name = 'emensawerbeseite';
+$db_port = 3306; // default MariaDB port
+
+return [
+    'host' => $db_host,
+    'user' => $db_user,
+    'password' => $db_pass,
+    'database' => $db_name,
+    'port' => $db_port
+];
